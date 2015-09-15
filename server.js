fe4da3b52505aac436e62599810eeec7f849bdea
jbride <jeff bride>
2015-09-15 05:19:00 -0600

    jenkins is already listening on 8080.  switching back to port 3000 so as to not conflict with jenkins

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.status(200).send('ok');
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(3000, function () {
   var port = server.address().port;
   console.log('Example app listening at port %s', port);
 });
