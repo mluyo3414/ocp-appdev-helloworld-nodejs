ddbd0ee3a2d1a34532e9af298e392cbfbea7eb51
miguel <mig.suarez49@gmail.com>
2017-10-12 13:43:59 -0500

    output2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.status(200).send('Welcome to Optum');
+  res.status(200).send('Welcome to Optum!');
 });
 
 var server = app.listen(8080, function () {
